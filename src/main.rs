bb802f546331384e38ba1fe9c3d81f41a8e40892
Tonmoy T. Bhattacharya <ultraton_16@yahoo.com>
2026-02-11 14:00:32 +0000

    added verbose ci file for rust tests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use sysinfo::{System};
-use tokio::time::{self, Duration};
-use serde::Serialize;
 use chrono::Utc;
+use serde::Serialize;
 use std::ffi::OsStr;
+use sysinfo::System;
+use tokio::time::{self, Duration};
 
 #[derive(Serialize, Debug)]
 struct Heartbeat {
@@ -15,16 +15,20 @@ struct Heartbeat {
 
 async fn collect_metrics(sys: &mut System) -> Heartbeat {
     sys.refresh_all();
-    
+
     // Check if a process named 'mysql' is running
     let is_mysql_up = sys.processes_by_name(OsStr::new("mysql")).next().is_some();
-    
+
     Heartbeat {
         timestamp: Utc::now().to_rfc3339(),
         node_id: "azure-mysql-node-01".to_string(),
         cpu_usage: sys.global_cpu_usage(),
         memory_used_kb: sys.used_memory(),
-        mysql_status: if is_mysql_up { "UP".to_string() } else { "DOWN".to_string() },
+        mysql_status: if is_mysql_up {
+            "UP".to_string()
+        } else {
+            "DOWN".to_string()
+        },
     }
 }
 
@@ -38,13 +42,12 @@ async fn main() {
     loop {
         interval.tick().await;
         let health_data = collect_metrics(&mut sys).await;
-        
+
         // TODO:: place into an API endpoint
         let json_payload = serde_json::to_string(&health_data).unwrap();
         println!("Sending Heartbeat: {}", json_payload);
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -54,6 +57,6 @@ mod tests {
 
     #[test]
     fn test_multiply() {
-        assert_eq!(6+6, 12);
+        assert_eq!(6 + 6, 12);
     }
 }
